bcc6110a47ab9f498f2fe9e6ef529bc8044b9c61
Dutta <701865@cognizant.com>
2020-02-19 20:13:26 +0530

    my first commit

---
+++ src/PushCode/CodePush.java
@@ -0,0 +1,12 @@
+package PushCode;
+
+public class CodePush {
+
+	public static void main(String[] args) {
+		
+	// TODO Auto-generated method stub
+		System.out.println("Push this code to GIT");
+
+	}
+
+}
